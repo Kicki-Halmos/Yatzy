db47e7afeecee1b46028f4181daa58c99c007a91
Kicki Halmos <kickihalmos@gmail.com>
2020-09-18 09:35:06 +0200

    changed css styling

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     
+
 }); 
 
-
     function calculate(event){
-
-        
-        event.preventDefault();
-        let sum = 0;
-        let tmp = 0;
-        let bonus = 0;
+     event.preventDefault();
+      
 
         for (let i = 1; i <= 4; i++) {
+            let sum = 0;
+            let tmp = 0;
+            let bonus = 0;
             for (let j = 1; j <= 6; j++){
                 tmp = document.getElementById("player" + i + "_" + j).value;
                 if (typeof(Number(tmp)) === "number") {
@@ -19,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function() {
                     if(sum >= 63){
                         bonus = 50;
                     }
-
+                  
                 } 
-            }
           
-            document.getElementById("player" + i + "_sum").value= sum;
-            document.getElementById("player" + i + "_bonus").value= bonus;
+            }
+            document.getElementById("player" + i + "_sum").innerHTML= sum;
+            document.getElementById("player" + i + "_bonus").innerHTML= bonus;
         }
     }
 
@@ -33,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("change", calculate);
 
     let button_1= document.getElementById("button_1");
-   let input = document.getElementById("input");
+    
 
     function throw_dice(event) {
         event.preventDefault();
@@ -43,10 +42,16 @@ document.addEventListener("DOMContentLoaded", function() {
         for (let i=1; i<=5; i++)
         {
             if (! document.getElementById("check_" +i).checked){
-            dice=Math.floor(Math.random() * 6) + 1;
-            document.getElementById("input_" + i ).value  = dice;
+            dice = Math.floor(Math.random() * 6) + 1;
+            for (let j=1; j<=6; j++){
+                let image = document.getElementById("img_" + i);
+                if (dice == j)
+                {
+                    image.src = "img/dice_" + j + ".png";
+                }
             }
         }
+    }
 
         
     }
@@ -55,47 +60,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-/*      
-     /* tmp = player1_1.value;
-        if (typeof(Number(tmp)) === "number") {
-            sum += Number(tmp);
-        }
 
-        tmp = player1_2.value;
-        if (typeof(Number(tmp)) === "number") {
-            sum += Number(tmp);
-        }
-
-        tmp = player1_3.value;
-        if (typeof(Number(tmp)) === "number") {
-            sum += Number(tmp);
-        }
-
-        tmp = player1_4.value;
-        if (typeof(Number(tmp)) === "number") {
-            sum += Number(tmp);
-        }
-
-        tmp = player1_5.value;
-        if (typeof(Number(tmp)) === "number") {
-            sum += Number(tmp);
-        }
-
-        tmp = player1_6.value;
-        if (typeof(Number(tmp)) === "number") {
-            sum += Number(tmp);
-        }
-
-        
-        sumField.value = sum;
-        
-        let bonus = 0;
-        if (sum >= 63) {
-            bonus = 50;
-        }
-        bonusField.value = bonus;
-
-    }*/
  
     
     
