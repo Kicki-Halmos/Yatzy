16e327535ca05a8b6d4cd755407eae825b1a4fa3
Kicki Halmos <kickihalmos@gmail.com>
2020-09-22 13:39:45 +0200

    fixat så att totalen uppdateras och resten av fälten i formuläret också summeras

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ function calculate(event) {
     event.preventDefault();
     for (let i = 1; i <= 4; i++) {
         let elements = document.getElementsByClassName("player" + i);
+        
+        
         let sum = 0;
         let bonus = 0;
+        let total = 0;
         for (let element of elements) {
             sum += Number(element.value);
         }
         if (sum >= 63) {
             bonus = 50;
         }
+        let the_rest = document.getElementsByClassName("player" + i + "_rest");  //resten av fältens input hämtas och summeras
+        for (let rest of the_rest){
+            total += Number(rest.value);
+        }
+        total = total + sum + bonus;
         document.getElementById("player" + i + "_sum").innerHTML = sum;
         document.getElementById("player" + i + "_bonus").innerHTML = bonus;
+        document.getElementById("player" + i + "_total").innerHTML = total;
     }
 }
 
@@ -65,6 +74,7 @@ function throw_dice(event) {
         }
         
     }
+
     if (remaining_throws.innerHTML == 0) {  //hoppar ur funktionen throw_dice
         return;
     }
