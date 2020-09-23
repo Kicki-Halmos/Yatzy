f361abb8fff8b4d86aae9968321bbe1fed4e5c1d
Kicki Halmos <kickihalmos@gmail.com>
2020-09-23 12:38:18 +0200

    fixed merge conflict 4real

--- script.js
+++ script.js
@@ -54,19 +54,10 @@ function throw_dice(event) {
 
     let remaining_throws = document.getElementById("remaining_throws");
 
-<<<<<<< HEAD
-    if (remaining_throws.innerHTML > 0) {
-        remaining_throws.innerHTML -= 1;
-       
-
-        // Minskar remaining throws med ett varje gång man klickar på knappen, och kör bara funktionen om remaining_throws är större än 0. 
-
-=======
     if (remaining_throws.innerHTML >= 0) { // Minskar remaining throws med ett varje gång man klickar på knappen,
         remaining_throws.innerHTML -= 1; //och kör bara funktionen om remaining_throws är större än 0. 
 
         //event.preventDefault(); - behövs ej efter att ha flyttat ut throw-knappen ur dice-form.
->>>>>>> origin/Thildes-ändringar
         let dice = 0;
         let diceResult = []; // skapar array för alla positioner tjarningar
         for (let i = 1; i <= 5; i++) {
@@ -87,65 +78,6 @@ function throw_dice(event) {
         updateTable(diceResult);        
     }
 
-<<<<<<< HEAD
-    if (remaining_throws.innerHTML == 0) {  //hoppar ur funktionen throw_dice
-        return;
-    }
-
-}
-
-
-
-button_1.addEventListener("click", throw_dice); //eventlyssnare som kör funktionen throw_dice
-
-
-
-//försök till att byta ut checkbox o klicka på bilder istället - fick det ej att funka
-/*
-for (let i = 1; i <= 5; i++) {
-    let dice_image = document.getElementById("img_" + i)
-    dice_image.addEventListener("click", function (event) {
-        event.preventDefault();
-        if (event.target.tagName != "IMG")
-            dice = Math.floor(Math.random() * 6) + 1;
-            for (let j = 1; j <= 6; j++) {
-
-            if (dice == j) {
-                dice_image.src = "img/dice_" + j + ".png";
-            }
-
-        }
-    });
-}
-
-*/
-
-let updateTable = function (diceResult) {
-  const result = countDice(diceResult);
-  
-  for (let i = 0; i < 6; i++) {
-    if (result[i] === 0) {
-        document.getElementById('player1_' + (i + 1)).value = '';
-    } else {
-        document.getElementById('player1_' + (i + 1)).value = result[i] * (i+1);
-    }
-  }
-}
-
-// Skapa nu array for values hur många tärningar som visar desamma prickar.
-let countDice = function(dice) {
-    let values = [];
-    
-    for (let i = 0; i <= 5; i++) {
-        values[i] = 0;
-    }
-    for (let current_dice of dice) {
-          values[current_dice - 1]++;
-    }
-    console.log(values);
-    return values;
-};
-=======
     if (remaining_throws.innerHTML == -1) { // När remaining throws är -1 så sätter vi om värdet på remaining throws till 3. 
         remaining_throws.innerHTML = 3; // I koden under så återställer vi tärningarna till 1 och gör icheckade boxar urcheckade. 
 
@@ -162,4 +94,3 @@ let countDice = function(dice) {
 
 
 button_1.addEventListener("click", throw_dice); //eventlyssnare som kör funktionen throw_dice
->>>>>>> origin/Thildes-ändringar
