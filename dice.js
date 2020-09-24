b20c63b8ed0408bf7108eb33721ee1a4566244ab
Mariia Paraketsova <paraketsova@gmail.com>
2020-09-24 19:28:28 +0200

    Remove unnecessary loop in random_throw()

--- dice.js
+++ dice.js
@@ -7,19 +7,14 @@ class Dice {
         for (let element of this.dice) {
             if (!document.getElementById("check_" + element).checked) {
                 die = Math.floor(Math.random() * 6) + 1;
-                for (let j = 1; j <= 6; j++) {
-                    let image = document.getElementById("img_" + element);
-                    if (die == j) {
-                        image.src = "img/dice_" + j + ".png";
-                    }
-                }
+                let image = document.getElementById("img_" + element);
+                image.src = "img/dice_" + die + ".png";
                 //diceResult.push(dice); // push nya values till array.
             } //else {
                 //diceResult.push(lastDiceResult[i-1]); // push nya values till array.
             //}
         }
         //lastDiceResult = diceResult.slice();
-        //updateTable(diceResult);        
-        //updateTable(diceResult);        
+        //updateTable(diceResult);     
     }
 }
