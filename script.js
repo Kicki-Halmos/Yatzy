0130d644b326235f5d751f17fd059c3e76a1bf10
Mathilde Åkerlind <mathilde.akerlind@gmail.com>
2020-09-24 22:43:05 +0200

    Snyggade till kommentarer

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let game = new Game();
     let button_1 = document.getElementById("button_1");
 
-    button_1.addEventListener("click", throw_dice); 
+    button_1.addEventListener("click", throw_dice);
     document.addEventListener("change", function(event) {
 
-        game.calculate_sum();     // kallar på klassen game och funktioner som beräknar summa, bonus och totalsumma
+        game.calculate_sum(); // kallar på klassen game och funktioner som beräknar summa, bonus och totalsumma.
         game.calculate_bonus();
         game.calculate_the_rest();
 
@@ -14,29 +14,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function throw_dice(event) {
     let dice = new Dice();
-    dice.random_throw();   // kallar på klassen dice och funktion för att slumpa tärningar
+    dice.random_throw(); // Kallar på klassen dice och funktion för att slumpa tärningar.
 
     let remaining_throws = document.getElementById("remaining_throws");
 
     if (remaining_throws.innerHTML >= 0) { // Minskar remaining throws med ett varje gång man klickar på knappen,
-                                            // och kör bara funktionen om remaining_throws är större än 0. 
-        
-        remaining_throws.innerHTML -= 1;      
+        // och kör bara funktionen om remaining_throws är större än 0. 
+
+        remaining_throws.innerHTML -= 1;
     }
 
-    if (remaining_throws.innerHTML == 0) {  
+    if (remaining_throws.innerHTML == 0) {
         let button_1 = document.getElementById("button_1");
         button_1.innerHTML = "Next player!";
     }
 
-    if (remaining_throws.innerHTML == -1) {  //återställer remaining throws
-        remaining_throws.innerHTML = 3;   
+    if (remaining_throws.innerHTML == -1) { // Återställer remaining throws.
+        remaining_throws.innerHTML = 3;
 
         let button_1 = document.getElementById("button_1");
         button_1.innerHTML = "Throw!";
-        
+
         for (let j = 1; j <= 5; j++) {
-            let image = document.getElementById("img_" + j);  //återställer tärningarna och ckeckar ur boxarna. 
+            let image = document.getElementById("img_" + j); // Återställer tärningarna och ckeckar ur boxarna. 
             image.src = "img/dice_" + 1 + ".png";
             if (document.getElementById("check_" + j).checked) {
                 document.getElementById("check_" + j).checked = false;
@@ -44,4 +44,3 @@ function throw_dice(event) {
         }
     }
 }
-
